8e58e31594d8e10eeac11c0e4d792b6daaf862fa
Ranshi <782938005@qq.com>
2021-12-07 21:56:23 +0800

    ✨ feat(script.py): 非命令行方式启动脚本
    添加了script.py 文件, 用于直接在编辑器中使用脚本, 方便调试新功能.
    提供了非命令行形式的使用方式, 避免命令行形式过于复杂而降低使用效率.

---
+++ script.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+# @Time     : 2021/12/07 21:12
+# @Author   : Ranshi
+# @File     : script.py
+# @Doc      : 非命令行形式的运行方式, 直接以脚本调用形式工作.
+
+from tools import split_pdf
+
+split_pdf(
+    source_path="/Users/rs/Downloads/Gutiérrez-Rodríguez et al_2016_SR of CCFP in China_EnvEvid.pdf",
+    mod="step",
+    arg=11,
+)
